124342f3c49cdf238dc3175cead3e28f886e2a5f
Pedro Manso <pedrolucasmppe@gmail.com>
2023-12-27 20:25:46 -0300

    Layer do xadrez

---
+++ src/chess/ChessMatch.java
@@ -0,0 +1,20 @@
+package chess;
+import chessboard.Board;
+
+public class ChessMatch {
+    private Board board;
+
+    public ChessMatch() {
+        board = new Board(8,8);
+    }
+
+    public ChessPiece[][] getPieces() {
+        ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColums()];
+        for (int i = 0; i < board.getRows(); i++) {
+            for (int j = 0; i < board.getColums(); i++) {
+                mat[i][j] = (ChessPiece) board.piece(i, j);
+            }
+        }
+        return mat;
+    }
+}
